af5d05e7e177844c394f9018991ea09b060e028a
rajaazan69 <azanr7260@gmail.com>
2025-08-17 01:36:20 +0500

    Create db.py

---
+++ utils/db.py
@@ -0,0 +1,23 @@
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+
+_mongo = None
+_db = None
+
+async def get_db():
+    global _mongo, _db
+    if _db:
+        return _db
+    uri = os.getenv("MONGO_URI")
+    _mongo = AsyncIOMotorClient(uri)
+    _db = _mongo["ticketbot"]
+    return _db
+
+async def collections():
+    db = await get_db()
+    return {
+        "tags": db["tags"],
+        "tickets": db["tickets"],
+        "transcripts": db["transcripts"],
+        "clientPoints": db["clientPoints"],
+    }
