e065c3b3c2d193f296d50b5e2c7558240af993f5
rajaazan69 <azanr7260@gmail.com>
2025-08-17 05:45:58 +0500

    Update Ticketpoints.py

--- cogs/Ticketpoints.py
+++ cogs/Ticketpoints.py
@@ -62,3 +62,7 @@ class TicketPoints(commands.Cog):
                     print("❌ Error updating leaderboard:", e)
 
         return user_ids  # Return who got points for followup message
+        
+        
+        async def setup(bot):
+    await bot.add_cog(TicketPoints(bot))
